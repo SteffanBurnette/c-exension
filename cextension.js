7ed9dcd21e6af7043b70bb18b0816454982c6a26
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-02-09 16:44:56 -0500

    add

---
+++ cextension.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+
+//Using an event listener to trigger  a function when the button is clicked
+//Specified the type of event we are listening for
+//Then created the fuction that we want to execute once the event is inoked
+btn.addEventListener("click", function() {
+    console.log("Clicked from the event listener")
+    
+})
