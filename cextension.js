b3c39e97182248ae6d05e3e0297627d72b71e825
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-02-11 14:43:37 -0500

    add

--- cextension.js
+++ cextension.js
@@ -3,32 +3,22 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const listEl = document.getElementById("list-el")
 
+
 //Using an event listener to trigger  a function when the button is clicked
 //Specified the type of event we are listening for
 //Then created the fuction that we want to execute once the event is inoked
 btn.addEventListener("click", function() {
     console.log("Clicked from the event listener")
     myLeads.push(inputEl.value)
+    renderLeads()
+    inputEl.value = "" //Clears the input field once its done
 
-    for(let i = 0; i < myLeads.length; i++){
-        console.log(myLeads[i])
-        listEl.innerHTML += "<li>" + myLeads[i] + "</li> "
-    }
-    
     
 })
 
-/*
-for(let i = 0; i < myLeads.length; i++){
-        console.log(myLeads[i])
-        listEl.innerHTML += "<li>" + myLeads[i] + "</li> "
-    }
-        */
+function renderLeads(){
 
-for(let i = 0; i < myLeads.length; i++){
-    
-    //Creates a html element 
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    listEl.append(li)
+    let listItems = "<li>" + inputEl.value + "</li> "
+    listEl.innerHTML += listItems
+
 }
