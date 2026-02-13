246c66b3930b0dd24d9c5c75949fff9def88e76a
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-02-13 14:37:41 -0500

    add

--- cextension.js
+++ cextension.js
@@ -23,7 +23,7 @@ if (leadsFromLocalStorage){
 }
 
 //Using an event listener to trigger  a function when the button is clicked
-//Specified the type of event we are listening for
+//Specified the type of event we are listening for.
 //Then created the fuction that we want to execute once the event is inoked
 btn.addEventListener("click", function() {
     console.log("Clicked from the event listener")
